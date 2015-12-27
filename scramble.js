ca08110bda7b751d3644825ece8c16120d1dbb22
Luke Ewer <ewer.wtsonline@gmail.com>
2015-12-27 21:11:18 +0000

    Implement basic random letter scrolling

--- scramble.js
+++ scramble.js
@@ -1,35 +1,32 @@
 var d = document;
 var el = d.getElementsByTagName('h2')[0];
 
-//el.style.visibility = "hidden";
+var possibleStrings = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "(", ")", "&hellip;"]
 
-//var finalStr = el.innerHTML;
-//el.innerHTML = "";
+//cycleString(el, "C", possibleStrings, 20);
+console.log(el.textContent);
+tagChars(el);
 
-//console.log(finalStr);
-//console.log(randomChar());
-/*
-for (var i = 0; i < finalStr.length; i++) {
-	(function(j){
-		setTimeout(function() {
-			el.innerHTML += randomChar(finalStr.split(''));
-		}, 50 * j);
-	})(i);
+var spans = el.getElementsByTagName('span');
+for (i = 0; i < spans.length; i++) {
+	cycleString(spans[i], spans[i].textContent, possibleStrings, 50);
 }
-*/
 
-
-var possibleStrings = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "(", ")"]
-
-cycleString(el, "C", possibleStrings, 20);
-console.log(el.innerHTML);
-/*
-function tagLetters(el, prepend, append) {
+function tagChars(el, prepend, append) {
 	var tagPrepend = prepend || "<span>";
 	var tagAppend = append || "</span>";
-	var arr = el.innerHTML.split
+	var arr = el.textContent.split("");
+
+	arr.forEach(function(value, i, arr) {
+		if (value !== " ") {
+			arr[i] = tagPrepend + value + tagAppend;
+		}
+	});
+
+	el.innerHTML = arr.join(""); 
 }
-*/
+
+
 function cycleString(el, targetStr, possibleStrs, cycleDelay) {
 	var interval = setInterval(function() {
 		el.innerHTML = randomString(possibleStrs);
